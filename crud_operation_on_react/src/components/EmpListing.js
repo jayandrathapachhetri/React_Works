164df07834023d9cd85d76c9f799d1773359efff
Jayandra <jayandrathapa5518@gmail.com>
2023-06-22 11:06:10 +0545

    EmpListing successfully create

--- crud_operation_on_react/src/components/EmpListing.js
+++ crud_operation_on_react/src/components/EmpListing.js
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 const EmpListing = () => {
+  const [empdata, empdatachange] = useState(null);
+  useEffect(() => {
+    fetch( `http://localhost:3000/employee`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        empdatachange(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
   return (
     <div className="container">
       <div className="card">
@@ -6,6 +21,7 @@ const EmpListing = () => {
           <h2>Employee Listing</h2>
         </div>
         <div className="card-body">
+          <div className="btn btn-success"> Add New (+)</div>
           <table className="table table-bordered">
             <thead className="bg-dark text-white">
               <tr>
@@ -16,6 +32,28 @@ const EmpListing = () => {
                 <td>Action</td>
               </tr>
             </thead>
+            <tbody>
+              {empdata &&
+                empdata.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>
+                      <a href="edit" className="btn btn-success">
+                        Edit
+                      </a>
+                      <a href="remove" className="btn btn-danger">
+                        Remove
+                      </a>
+                      <a href="details" className="btn btn-primary">
+                        Details
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
           </table>
         </div>
       </div>
