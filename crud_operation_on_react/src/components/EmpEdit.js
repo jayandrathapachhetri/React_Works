9f3d4ef5a5dd7cfc5e6c42ef99d2330ecedfa06d
Jayandra <jayandrathapa5518@gmail.com>
2023-06-22 14:10:12 +0545

    Upadte work Done

--- crud_operation_on_react/src/components/EmpEdit.js
+++ crud_operation_on_react/src/components/EmpEdit.js
@@ -7,7 +7,7 @@ const EmpEdit = () => {
     //const [empdata, empdatachange] = useState({});
 
     useEffect(() => {
-        fetch("http://localhost:8000/employee/" + empid).then((res) => {
+        fetch("http://localhost:3000/employee/" + empid).then((res) => {
             return res.json();
         }).then((resp) => {
             idchange(resp.id);
@@ -18,14 +18,15 @@ const EmpEdit = () => {
         }).catch((err) => {
             console.log(err.message);
         })
-    }, []);
+    }, [empid]);
 
     const[id,idchange]=useState("");
     const[name,namechange]=useState("");
     const[email,emailchange]=useState("");
     const[phone,phonechange]=useState("");
     const[active,activechange]=useState(true);
-    const[validation,valchange]=useState(false);
+    const[namevalidation,valnamechange]=useState(false);
+    const[emailvalidation,valemailchange]=useState(false);
 
 
     const navigate=useNavigate();
@@ -35,12 +36,12 @@ const EmpEdit = () => {
       const empdata={id,name,email,phone,active};
       
 
-      fetch("http://localhost:8000/employee/"+empid,{
+      fetch("http://localhost:3000/employee/"+empid,{
         method:"PUT",
         headers:{"content-type":"application/json"},
         body:JSON.stringify(empdata)
       }).then((res)=>{
-        alert('Saved successfully.')
+        alert('Update successfully.')
         navigate('/');
       }).catch((err)=>{
         console.log(err.message)
@@ -48,73 +49,94 @@ const EmpEdit = () => {
 
     }
     return ( 
-        <div>
-
-        <div className="row">
-            <div className="offset-lg-3 col-lg-6">
-                <form className="container" onSubmit={handlesubmit}>
-
-                    <div className="card" style={{"textAlign":"left"}}>
-                        <div className="card-title">
-                            <h2>Employee Edit</h2>
-                        </div>
-                        <div className="card-body">
-
-                            <div className="row">
-
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>ID</label>
-                                        <input value={id} disabled="disabled" className="form-control"></input>
-                                    </div>
-                                </div>
-
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>Name</label>
-                                        <input required value={name} onMouseDown={e=>valchange(true)} onChange={e=>namechange(e.target.value)} className="form-control"></input>
-                                    {name.length===0 && validation && <span className="text-danger">Enter the name</span>}
-                                    </div>
-                                </div>
-
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>Email</label>
-                                        <input value={email} onChange={e=>emailchange(e.target.value)} className="form-control"></input>
-                                    </div>
-                                </div>
-
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>Phone</label>
-                                        <input value={phone} onChange={e=>phonechange(e.target.value)} className="form-control"></input>
-                                    </div>
-                                </div>
-
-                                <div className="col-lg-12">
-                                    <div className="form-check">
-                                    <input checked={active} onChange={e=>activechange(e.target.checked)} type="checkbox" className="form-check-input"></input>
-                                        <label  className="form-check-label">Is Active</label>
-                                        
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                       <button className="btn btn-success" type="submit">Save</button>
-                                       <Link to="/" className="btn btn-danger">Back</Link>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                        </div>
-
+<div>
+      <div className="row">
+        <div className="offset-lg-3 col-lg-6">
+          <form className="container" onSubmit={handlesubmit}>
+            <div className="card" style={{ textAlign: "left" }}>
+              <div className="card-title">
+                <h2>Employee Create</h2>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>ID</label>
+                      <input
+                        value={id}
+                        disabled="disabled"
+                        className="form-control"
+                      ></input>
                     </div>
+                  </div>
 
-                </form>
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Name</label>
+                      <input
+                        required
+                        value={name}
+                        onMouseDown={(e) => valnamechange(true)}
+                        onChange={(e) => namechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                      {name.length === 0 && namevalidation && (
+                        <span className="text-danger">Enter the valid name</span>
+                      )}
+                    </div>
+                  </div>
 
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Email</label>
+                      <input
+                        value={email}
+                        onMouseDown={(e) => valemailchange(true)}
+                        onChange={(e) => emailchange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                      {email.length === 0 && emailvalidation && (<span className="text-danger"> Enter the vaild email </span>)}
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Phone</label>
+                      <input
+                        value={phone}
+                        onChange={(e) => phonechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-check">
+                      <input
+                        checked={active}
+                        onChange={(e) => activechange(e.target.checked)}
+                        type="checkbox"
+                        className="form-check-input"
+                      ></input>
+                      <label className="form-check-label">Is Active</label>
+                    </div>
+                  </div>
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <button className="btn btn-success" type="submit">
+                        Update
+                      </button>
+                      <Link to="/" className="btn btn-danger">
+                        Back
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </form>
         </div>
+      </div>
     </div>
      );
 }
