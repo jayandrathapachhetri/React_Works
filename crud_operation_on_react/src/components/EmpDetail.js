4ec7dac21949a8ec87fd68bba26f68a62a12b937
Jayandra <jayandrathapa5518@gmail.com>
2023-06-22 13:02:30 +0545

    import and state variable define

--- crud_operation_on_react/src/components/EmpDetail.js
+++ crud_operation_on_react/src/components/EmpDetail.js
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
 const EmpDetail = () => {
+    const {empid} = useParams();
+    // empty object
+    const[empdata, setdatachange] = useState({});
+
     return(
         <div>
             <h1>hello</h1>
