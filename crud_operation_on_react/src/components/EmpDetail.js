77e6e2c96189cbe652086995b73cdc4016bb31fb
Jayandra <jayandrathapa5518@gmail.com>
2023-06-22 13:04:22 +0545

    emp detail entry

--- crud_operation_on_react/src/components/EmpDetail.js
+++ crud_operation_on_react/src/components/EmpDetail.js
@@ -8,8 +8,29 @@ const EmpDetail = () => {
 
     return(
         <div>
-            <h1>hello</h1>
-        </div>
-    )
-}
+           
+
+               <div className="container">
+                
+            <div className="card row" style={{ "textAlign": "left" }}>
+                <div className="card-title">
+                    <h2>Employee Create</h2>
+                </div>
+                <div className="card-body"></div>
+
+                {empdata &&
+                    <div>
+                        <h2>The Employee name is : <b>{empdata.name}</b>  ({empdata.id})</h2>
+                        <h3>Contact Details</h3>
+                        <h5>Email is : {empdata.email}</h5>
+                        <h5>Phone is : {empdata.phone}</h5>
+                        <Link className="btn btn-danger" to="/">Back to Listing</Link>
+                    </div>
+                }
+            </div>
+            </div>
+            {/* </div>
+            </div> */}
+        </div >
+    );
 export default EmpDetail;
