1eacf2df5022342e599ff3aa1b8f63f718a0f327
Jayandra <jayandrathapa5518@gmail.com>
2023-07-11 14:48:57 +0545

    TextUtils Project

---
+++ textutils/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import "./App.css";
+import Navbar from "./Navbar.js";
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar title="TextUtils" aboutText="About TextUtils"/>
+    </div>
+  );
+}
+
+export default App;
