3a55f0aac249cc2b9ce1065975306adaefcbbf14
Jayandra <jayandrathapa5518@gmail.com>
2023-07-13 13:33:05 +0545

    challenge accept

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,43 +1,122 @@
-import React, {useState}  from "react";
+// import React, {useState}  from "react";
+
+// export default function Textform(props) {
+//   const [text, setText] = useState("Enter Text here");
+
+//   const handleOnChange = (event) => {
+//     // console.log("On change");
+//     setText(event.target.value);
+// }
+
+//   const handleUpClick = () => {
+//     // console.log("Uppercase was clicked");
+//     let newText = text.toUpperCase();
+//     setText(newText)
+//   }
+
+//   const handleLoClick = () => {
+//     // console.log("Lowercase was clicked");
+//     let newText = text.toLowerCase();
+//     setText(newText)
+//   }
+
+
+//   return (
+//     <>
+//     <div className="container">
+//       <h1>{props.heading}</h1>
+//       <div className="mb-3">
+//         <textarea className="form-control" value={text} onChange={handleOnChange} id='myBox' rows="8"></textarea>
+//         <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to Uppercase</button>
+//         <button className="btn btn-primary mx-1" onClick={handleLoClick} >Convert to Lowercase</button>
+//       </div>
+//     </div>
+//     <div className="container my-3">
+//         <h2>Your text Summary</h2>
+//         <p>{text.split(" ").length} words and {text.length} characters</p>
+//         <p>{0.008 * text.split(" ").length} Minutes read</p>
+//         <h2>Preview</h2>
+//         <p>{text}</p>
+//     </div>
+//     </>
+//   );
+// }
+
+import React, { useState } from "react";
 
 export default function Textform(props) {
   const [text, setText] = useState("Enter Text here");
+  const [editing, setEditing] = useState(false);
 
   const handleOnChange = (event) => {
-    // console.log("On change");
     setText(event.target.value);
-}
+  };
 
   const handleUpClick = () => {
-    // console.log("Uppercase was clicked");
     let newText = text.toUpperCase();
-    setText(newText)
-  }
+    setText(newText);
+  };
 
   const handleLoClick = () => {
-    // console.log("Lowercase was clicked");
     let newText = text.toLowerCase();
-    setText(newText)
-  }
+    setText(newText);
+  };
 
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    setEditing(false);
+  };
 
   return (
     <>
-    <div className="container">
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id='myBox' rows="8"></textarea>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to Uppercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleLoClick} >Convert to Lowercase</button>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          {editing ? (
+            <textarea
+              className="form-control"
+              value={text}
+              onChange={handleOnChange}
+              id='myBox'
+              rows="8"
+            ></textarea>
+          ) : (
+            <p>{text}</p>
+          )}
+          {editing ? (
+            <>
+              <button className="btn btn-primary mx-1 my-2" onClick={handleSaveClick}>
+                Save
+              </button>
+            </>
+          ) : (
+            <>
+              <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+                Convert to Uppercase
+              </button>
+              <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+                Convert to Lowercase
+              </button>
+              <button className="btn btn-primary mx-1" onClick={handleEditClick}>
+                Edit
+              </button>
+            </>
+          )}
+        </div>
       </div>
-    </div>
-    <div className="container my-3">
+      <div className="container my-3">
         <h2>Your text Summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text}</p>
-    </div>
+      </div>
     </>
   );
 }
+
