1eb0ce97b9e833253b3967ed4f3835408224b17f
Jayandra <jayandrathapa5518@gmail.com>
2023-07-13 12:46:46 +0545

    toUppercase Done

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState}  from "react";
 
 export default function Textform(props) {
+  const [text, setText] = useState("Enter Text here");
+
+  const handleOnChange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+}
+
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText)
+}
   return (
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" id="'myBox" rows="8"></textarea>
-        <button className="btn btn-primary">Convert to Uppercase</button>
+        <textarea className="form-control" value={text} onChange={handleOnChange} id='myBox' rows="8"></textarea>
+        <button className="btn btn-primary" onClick={handleUpClick} >Convert to Uppercase</button>
       </div>
     </div>
   );
