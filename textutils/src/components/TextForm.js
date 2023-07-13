d3efa568422c9bcda9533353b2951d89050f9076
Jayandra <jayandrathapa5518@gmail.com>
2023-07-13 13:10:45 +0545

    Adding more Logic to TextUtils

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -14,12 +14,19 @@ export default function Textform(props) {
     setText(newText)
 }
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id='myBox' rows="8"></textarea>
         <button className="btn btn-primary" onClick={handleUpClick} >Convert to Uppercase</button>
       </div>
     </div>
+    <div className="container my-3">
+        <h2>Your text Summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+    </div>
+    </>
   );
 }
