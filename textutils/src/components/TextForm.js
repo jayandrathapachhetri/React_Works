ac952a3655637006c740d5350cf446485ffc58af
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-07-16 09:35:31 +0545

    Word counting error solved

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -56,7 +56,7 @@ export default function Textform(props) {
     </div>
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#153003'}}>
         <h2>Your text Summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something in the textbox above to preview it here."}</p>
