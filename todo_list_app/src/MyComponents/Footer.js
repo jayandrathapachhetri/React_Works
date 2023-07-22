ddbd9f26117b42ce11772b3dbd000de8ddb845e7
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-07-22 08:36:54 +0545

    LocalStorage use

--- todo_list_app/src/MyComponents/Footer.js
+++ todo_list_app/src/MyComponents/Footer.js
@@ -4,11 +4,12 @@ import React from 'react'
 function Footer(){ 
     let footerStyle = {
         position: "relative",
-        top: "5vh",
+        top: "36vh",
         width: "100%",
     }
+    
     return (
-        <footer className="bg-dark text-light py-3" style={footerStyle}>
+        <footer className="bg-dark text-light py-3" style={footerStyle} > 
             <p className="text-center text-height-sm">
             Copyright &copy; MyTodosList.com
             </p>
--- todo_list_app/src/MyComponents/TodoItem.js
+++ todo_list_app/src/MyComponents/TodoItem.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function TodoItem(props) {
   return (
     <div>
-      {/* <h2>todoitem</h2> */}
       <p>{props.Todo.title} and {props.Todo.desc}</p>
       <button className='btn btn-danger btn-sm mb-4' onClick={()=>{props.onDelete(props.Todo)}}>Delete</button>
     </div>
