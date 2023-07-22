56ade9a3c8f3e13eb1c75a85fb4076ca0e709313
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-07-22 10:50:39 +0545

    Almost Complete

--- todo_list_app/src/App.js
+++ todo_list_app/src/App.js
@@ -2,15 +2,34 @@ import "./App.css";
 import Footer from "./MyComponents/Footer";
 import Header from "./MyComponents/Header";
 import Todos from "./MyComponents/Todos";
+import About from "./MyComponents/About";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 
 function App() {
   return (
-    <div className="App">
-      <Header title="Todo List" searchBar={true} />
-      <Todos/>
+    // <div className="App">
+    //   <Header title="Todo List" searchBar={true} />
+    //   <Todos/>
+    //   <Footer />
+    // </div>
+    <>
+    <Router>
+      <Header title="Todos List" searchBar={true} /> 
+      <Routes>
+          <Route exact path="/" element={<Todos/>} />
+          
+          <Route path="/about" element={<About/>} />
+        </Routes> 
       <Footer />
-    </div>
+    </Router>
+    </>
   );
 }
 
--- todo_list_app/src/MyComponents/About.js
+++ todo_list_app/src/MyComponents/About.js
@@ -1,10 +1,51 @@
 import React from 'react'
 
-export const About = () => {
-    return (
-        <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+function About() {
+  return (
+    <>
+   <div className="container">
+            <h1 className="my-3">About Us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <strong>App Purpose</strong>
+                    </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
+                    The Todo List app is designed to help users manage their tasks and stay organized by creating and tracking a list of things they need to do.
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item" >
+                    <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <strong>Free to use</strong>
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" >
+                    The Todo List app is completely free to use, allowing users to organize their tasks without any cost or subscription fees.
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed"  type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <strong>Browser Compatible</strong>
+                    </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
+                    The Todo List app is designed to be compatible with various web browsers, ensuring seamless access and functionality across different platforms.
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+</>
+  )
 }
+
+export default About
