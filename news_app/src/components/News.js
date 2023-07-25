ebbd903a132b94acaef123a13598f85410d49b10
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-07-25 07:21:33 +0545

    Adding variable pageSize

--- news_app/src/components/News.js
+++ news_app/src/components/News.js
@@ -2,32 +2,97 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  constructor(){
+  constructor(props) {
     super();
-    this.state ={
-      articles : [], // Initialize articles as an empty array
+    this.state = {
+      articles: [], // Initialize articles as an empty array
       loading: false,
-      page: 1
-    }
+      page: 1,
+      totalArticles: 0, // Initialize totalArticles as 0
+    };
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=f31ca37e0d664a5b87ffa95ec0b79dd4";
+  async componentDidMount() {
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=f31ca37e0d664a5b87ffa95ec0b79dd4&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
-    let paresdData = await data.json()
-    console.log(paresdData);
-    this.setState({articles : paresdData.articles})
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalArticles: parsedData.totalResults, // Set the totalResults from the API response
+    });
   }
+
+  handlePrevClick = async () => {
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f31ca37e0d664a5b87ffa95ec0b79dd4&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  
+  handleNextClick = async () => {
+    console.log("Next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalArticles / this.props.pageSize)) {
+      // Handle the case when there are no more pages to fetch
+      return;
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f31ca37e0d664a5b87ffa95ec0b79dd4&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+  
+
   render() {
     return (
       <div className="container my-3">
         <h2>Amazing News</h2>
         <div className="row">
-          {this.state.articles.map((element)=>{
-            return <div className="col-md-4" key = {element.url}>
-            <NewsItem title = {element.title?element.title:""} description= {element.description?element.description:""} imageUrl= {element.urlToImage} newsUrl={element.url} />
-          </div>
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
           })}
+        </div>
+        <div className="container d-flex justify-content-between mt-3">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / this.props.pageSize) }
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- todo_list_app/src/MyComponents/Header.js
+++ todo_list_app/src/MyComponents/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function Header(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-light bg-light">
   <div className="container-fluid ">
     <Link className="navbar-brand" to="/">{props.title}</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
