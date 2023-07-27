d7ee4d1cd4b984668c6ce8cfb4f756bddc76e8c5
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-07-27 07:02:25 +0545

    Auther, Date and Badges Use

--- news_app/src/components/NewsItem.js
+++ news_app/src/components/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div my-3>
         
@@ -10,10 +10,11 @@ export class NewsItem extends Component {
         <img src={!imageUrl?"https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg":imageUrl} className="card-img-top" alt="..."/>
           {/* <img src={imageUrl} className="card-img-top" alt="..." /> */}
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+          <h5 className="card-title">{title} <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}> {source} </span>
             <p className="card-text">
-              {description}...
+              {description}
             </p>
+            <p className="card-text"><small className="text-muted">By {!author?"unknown":author} on {new Date (date).toGMTString()}</small></p>
             <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">
               Read More
             </a>
