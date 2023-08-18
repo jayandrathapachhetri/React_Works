c01d150e4fcd9af668fb378a8cf51e60d73a677c
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-08-18 19:39:08 +0545

    Contact page Done

--- tech_website/src/Contact.jsx
+++ tech_website/src/Contact.jsx
@@ -1,11 +1,82 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Contact() {
+  const Wrapper = styled.section`
+  padding: 9rem 0 5rem 0;
+
+  .container {
+    margin-top: 6rem;
+    text-align: center;
+
+    .contact-form {
+      max-width: 50rem;
+      margin: auto;
+
+      .contact-inputs {
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;  
+
+        input[type="submit"] {
+          cursor: pointer;
+          transition: all 0.2s;
+
+          &:hover {
+            background-color: ${({ theme }) => theme.colors.white};
+            border: 1px solid ${({ theme }) => theme.colors.btn};
+            color: ${({ theme }) => theme.colors.btn};
+            transform: scale(0.9);
+          }
+        }
+      }
+    }
+  }
+  `;
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
-  )
+    <Wrapper>
+      <h2 className="common-heading">feel free to contact us</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.3793445928854!2d85.33183547505068!3d27.705571525570555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb199ffe9d7c6b%3A0x91b3a969f305a0bc!2sMaitidevi%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1692364836593!5m2!1sen!2snp"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/xyyqwgak" method="POST" className="contact-inputs">
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              autoComplate="off"
+              required
+            />
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplate="off"
+              required
+            />
+
+            <textarea
+              name="message"
+              cols="30"
+              rows="7"
+              className="name"
+              required
+            ></textarea>
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
-export default Contact
+export default Contact;
